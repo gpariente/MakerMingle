aeab774a42d9f85623df164c35a215a2d9d91eb5
ofek <benavrahamofek@gmail.com>
2024-02-10 11:26:03 +0200

    fix the opening dialog in pages

---
+++ navbar-interactions.js
@@ -0,0 +1,16 @@
+function openPopupLogin() {
+    document.getElementById("overlay").style.display = "flex";
+}
+
+function closePopupLogin() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function openPopupSignUp() {
+    document.getElementById("overlaysignup").style.display = "flex";
+}
+
+function closePopupSignUp() {
+    document.getElementById("overlaysignup").style.display = "none";
+}
+
